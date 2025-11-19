6c5877e266ea93b4d3953e7cf2a3ce655e3893aa
Mauro Gentile <62186646+gents83@users.noreply.github.com>
2025-11-19 12:07:24 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,5 @@
-
-
+import '@angular/compiler';
+import { platformBrowser } from '@angular/platform-browser'; 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { provideZonelessChangeDetection } from '@angular/core';
